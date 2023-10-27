6060f15881cf8d3d649e93b42d15e77e75253426
Vincent Massaro <vincentmassaro@gmail.com>
2023-10-27 12:46:42 -0400

    restructure starterkit repo, add releases (#2)
    * chore: move files into starterkit directory
    
    * feat: add semantic-release with script to generate zip that is compliant with single_content_sync
    
    * chore: add comment to release script
    
    * chore: remove npm install step
    
    * fix: install semantic-release/exec
    
    * chore: change token
    
    * fix: file permissions
    
    * fix: update readme
    
    * fix: releaserc syntax issue
    
    * fix: only attach starterkit.zip
    
    * chore: remove .DS_Store files
    
    * chore: update secret
    
    * chore: remove non-working code

---
+++ .github/scripts/release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Generate a zip file with no extra subdirectory.
+# We do this to be make a zip that single_content_sync
+# can import correctly.
+
+mkdir tmp
+cd starterkit
+zip -r ../tmp/starterkit.zip .
